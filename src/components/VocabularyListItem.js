914eb3245758a3663f15e21c98a25b123ae4d2b0
Hyeonsu Lee <incleaf@gmail.com>
2017-09-26 14:00:04 +0900

    Update style

--- src/components/VocabularyListItem.js
+++ src/components/VocabularyListItem.js
@@ -5,12 +5,17 @@ import './VocabularyListItem.css';
 const VocabularyListItem = ({ id, value }) => {
   return (
     <li className="vocablistitem">
-      <a
-        href={`http://m.endic.naver.com/search.nhn?searchOption=all&query=${value}`}
-        target="_blank"
-      >
-        {value}
-      </a>
+      <div className="vocablistitem__value">
+        <a
+          href={`http://m.endic.naver.com/search.nhn?searchOption=all&query=${value}`}
+          target="_blank"
+        >
+          {value}
+        </a>
+      </div>
+      <button className="vocablistitem__btn-achieve">
+        Achieve
+      </button>
     </li>
   );
 };
