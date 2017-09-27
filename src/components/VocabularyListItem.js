d21c8325009d549428e1d2841bdcbf17640515d9
Hyeonsu Lee <incleaf@gmail.com>
2017-09-27 19:52:09 +0900

    Add edit, delete buttons

--- src/components/VocabularyListItem.js
+++ src/components/VocabularyListItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Delete, Edit } from './Icons';
 
 import './VocabularyListItem.css';
 
@@ -12,6 +13,14 @@ const VocabularyListItem = ({ id, value, isAchieved, handleButtonClick }) => {
         >
           {value}
         </a>
+      </div>
+      <div className="vocablistitem__extras">
+        <button className="vocablistitem__extra-item">
+          <Edit />
+        </button>
+        <button className="vocablistitem__extra-item">
+          <Delete />
+        </button>
       </div>
       <button className="vocablistitem__btn-achieve" onClick={handleButtonClick}>
         {isAchieved ? 'Undo' : 'Achieve'}
